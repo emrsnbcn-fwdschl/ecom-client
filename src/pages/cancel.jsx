4f74608db1af5161db62c5e474c2b1f69c544efe
Emerson <emerson@forwardschool.edu.my>
2023-05-24 14:49:07 +0800

    fix link

--- src/pages/cancel.jsx
+++ src/pages/cancel.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Cancel() {
   return (
     <div>
       <h2>Oops something went wrong...</h2>
-      <a href="/cart">Go back to My Cart</a>
+      <Link href="/">Go back</Link>
     </div>
   );
 }
